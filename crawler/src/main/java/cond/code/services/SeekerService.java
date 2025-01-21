cea11aeb44902a6364e651cca37987fc29a3b62a
March93B <maftycolors@gmail.com>
2025-01-21 14:07:37 -0300

    seeker

---
+++ crawler/src/main/java/cond/code/services/SeekerService.java
@@ -0,0 +1,20 @@
+package cond.code.services;
+
+import cond.code.entities.BlackDuck;
+import cond.code.entities.Seeker;
+
+import java.util.List;
+
+public interface SeekerService {
+    void createSeeker(Seeker seeker);
+    void updateSeeker(Seeker seeker);
+    void deleteSeeker(Integer id);
+    Seeker getBSeekerId(Integer id);
+    Seeker getSeekerByApiName(String apiName);
+    Seeker getSeekerByUrl(String url);
+    List<Seeker> getSeekers();
+    List<Seeker> getSeekersActiveProd();
+//    void foundyey(List<BlackDuck> blackDucks, String cookie, String cookie2, String envv, int a, int b) throws Exception;
+//    int getProgress();
+
+}
